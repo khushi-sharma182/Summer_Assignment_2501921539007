67be8f59e7b10ae53034ce6ed6a787fad43689f7
khushi-sharma182 <ds5074389@gmail.com>
2026-06-08 16:05:29 +0530

    Optimize ransomNote construction check
    Refactor canConstruct method to use frequency array for efficiency.

--- week_2/Day_1/ransomnote.cpp
+++ week_2/Day_1/ransomnote.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int r=ransomNote.length();
-        int m=magazine.length();
-        for(int j=0;j<r;j++){
-            for(int i=0;i<m;i++)
-            {
-                if(ransomNote[j]!=magazine[i] ){
-                    return false;
-                }
+        int freq[26] = {0};
+
+        for(char ch : magazine) {
+            freq[ch - 'a']++;
+        }
+
+        for(char ch : ransomNote) {
+            freq[ch - 'a']--;
+
+            if(freq[ch - 'a'] < 0) {
+                return false;
             }
         }
+
         return true;
     }
 };
