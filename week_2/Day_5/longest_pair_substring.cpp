eefa932f1b249a0fdfb775aa046f658615143839
khushi-sharma182 <ds5074389@gmail.com>
2026-06-12 22:32:22 +0530

    Create longest_pair_substring.cpp

---
+++ week_2/Day_5/longest_pair_substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    string expand(string s, int l, int r)
+    {
+        while(l >= 0 && r < s.size() && s[l] == s[r])
+        {
+            l--;
+            r++;
+        }
+        return s.substr(l + 1, r - l - 1);
+    }
+    string longestPalindrome(string s) {
+
+        string ans = "";
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            string p1 = expand(s, i, i);
+            string p2 = expand(s, i, i + 1);
+
+            if(p1.size() > ans.size())
+                ans = p1;
+
+            if(p2.size() > ans.size())
+                ans = p2;
+        }
+        return ans;
+    }
+};
